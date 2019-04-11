5e6fa2779b8514858de51be8e3f7f966ef04cde8
wl <wanglei@yeyisun.com>
2019-04-11 21:05:02 +0800

    update html

--- Public/static/js/main.js
+++ Public/static/js/main.js
@@ -1,4 +1,18 @@
 (function () {
+    $(".dropdown").bind('mouseenter', function(){
+        $this = $(this)
+        console.log('$this', $this[0].clientWidth)
+        $this.animate({
+            width: ($this[0].clientWidth * 2)+'px'
+        })
+    })
+    $(".dropdown").bind('mouseleave', function(){
+        $this = $(this)
+        console.log('$this', $this)
+        $this.animate({
+            width: ($this[0].clientWidth/2) + 'px'
+        })
+    })
     $("#imgPreModal").on("show.bs.modal", function(event){
         var button = $(event.relatedTarget);
         console.log('button', button.data)
