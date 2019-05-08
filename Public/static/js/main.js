00abfce01d86965e07cc48bc0767677165dd3d63
wl <wanglei@yeyisun.com>
2019-05-08 18:05:26 +0800

    update front

--- Public/static/js/main.js
+++ Public/static/js/main.js
@@ -13,13 +13,30 @@
             width: '110px'
         })
     })
-    $("#imgPreModal").on("show.bs.modal", function(event){
-        var button = $(event.relatedTarget);
-        console.log('button', button.data)
-        var srcImg = button.data('whatever');
-        console.log('srcImg', srcImg)
-        var modal = $(this);
-        modal.find(".showPreImg")[0].src = srcImg
-        console.log('modal.find(".showPreImg")', modal.find(".showPreImg")[0])
+    $("#lab-sence .swiper-container .swiper-wrapper .swiper-slide>img").on('mouseenter', function(e){
+        // $("#imgPreModal").addClass("in");
+        var $this = $(this)
+        var imgModal = $('#imgPreModal')
+        imgModal.modal('show')
+        console.log('e====',e)
+        console.log('$this', $this)
+        var srcImg = $this[0].currentSrc
+        imgModal.find(".showPreImg")[0].src = srcImg
+        // console.log('==================', $this)
     })
+    // $("#lab-sence .swiper-container .swiper-wrapper .swiper-slide>img").on('mouseleave', function(){
+    //     $this = $(this)
+    //     $("#imgPreModal").removeClass("in");
+    //     console.log('==================', $this)
+    // })
+    // $("#imgPreModal").on("show.bs.modal", function(event){
+    //     console.log('=====================',event)
+    //     var button = $(event.relatedTarget);
+    //     console.log('button', button.data)
+    //     var srcImg = button.data('whatever');
+    //     console.log('srcImg', srcImg)
+    //     var modal = $(this);
+    //     modal.find(".showPreImg")[0].src = srcImg
+    //     console.log('modal.find(".showPreImg")', modal.find(".showPreImg")[0])
+    // })
 }());
