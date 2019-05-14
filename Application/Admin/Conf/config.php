ad011358c6bdce85a285601500f0a78dd384f3e9
KhanMxp <khan_mxp@163.com>
2019-05-14 18:49:07 +0800

    'daoahang'

--- Application/Admin/Conf/config.php
+++ Application/Admin/Conf/config.php
@@ -1,20 +1,20 @@
 <?php
 return array(
     //数据库配置'配置项'=>'配置值'
-    'DB_TYPE'=>'mysql',
-    'DB_HOST'=>'10.112.66.2',
-    'DB_NAME'=>'jcweb',
-    'DB_USER'=>'admin',
-    'DB_PWD'=>'admin',
-    'DB_PORT'=>'3306',
-    'DB_PREFIX' => '',
 //    'DB_TYPE'=>'mysql',
-//    'DB_HOST'=>'localhost',
+//    'DB_HOST'=>'10.112.66.2',
 //    'DB_NAME'=>'jcweb',
-//    'DB_USER'=>'root',
-//    'DB_PWD'=>'',
+//    'DB_USER'=>'admin',
+//    'DB_PWD'=>'admin',
 //    'DB_PORT'=>'3306',
 //    'DB_PREFIX' => '',
+    'DB_TYPE'=>'mysql',
+    'DB_HOST'=>'localhost',
+    'DB_NAME'=>'jcweb',
+    'DB_USER'=>'root',
+    'DB_PWD'=>'',
+    'DB_PORT'=>'3306',
+    'DB_PREFIX' => '',
     //分页参数配置
     'Page' => array(
         'page_num' => 10,
